6e3cdfe66d584718b4353133fca4121ed07cb83e
Chris Van Huffe <cvanhuffel@outlook.com>
2020-12-15 16:28:34 -0800

    Disabled gatsby-node file

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,12 +38,14 @@ module.exports = {
       }
     },
 
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
+    // {
+    //   resolve: `gatsby-plugin-netlify`,
+    //   options: {
         
-      },
-    },
+    //   },
+    // },
+
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
